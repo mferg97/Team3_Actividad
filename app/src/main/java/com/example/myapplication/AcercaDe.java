a6f6a2db6704b89c4f79303675e46c8ef0305d72
Alumno1 <alumno1@anahuacmayab.edu.mx>
2019-10-09 19:05:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/AcercaDe.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AcercaDe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acerca_de);
+
+
+            //Importamos texto de Act.1
+            Bundle bundle = getIntent().getExtras();
+            String fraseimportada=bundle.getString("frase");
+
+            String textoPasado = fraseimportada;
+            TextView out = (TextView)findViewById(R.id.textoPasado);
+            out.setText(textoPasado);
+
+            Button btn2 = (Button) findViewById(R.id.botonact2);
+            btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent2 = new Intent (v.getContext(), MainActivity.class);
+                    startActivityForResult(intent2, 0);
+
+                }
+            });
+        }
+    }
